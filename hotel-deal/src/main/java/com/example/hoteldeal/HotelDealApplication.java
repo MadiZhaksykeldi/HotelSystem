fa8dd420cff3ac1a993f644274bcb43016106277
Madi <madizhaksigeldi@gmail.com>
2020-09-26 20:36:52 +0600

    Initial commit

---
+++ hotel-deal/src/main/java/com/example/hoteldeal/HotelDealApplication.java
@@ -0,0 +1,11 @@
+package com.example.hoteldeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDealApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelDealApplication.class, args);
+    }
+}
---
+++ hotel-support/src/main/java/com/example/hotelsupport/HotelSupportApplication.java
@@ -0,0 +1,11 @@
+package com.example.hotelsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSupportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelSupportApplication.class, args);
+    }
+}
